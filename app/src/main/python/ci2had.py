bb57740c92fbc023d688bfc69710ba982edf498b
John W Carlson <yottzumm@gmail.com>
2024-10-05 08:08:53 -0500

    Updated

--- app/src/main/python/ci2had.py
+++ app/src/main/python/ci2had.py
@@ -26,6 +26,42 @@ component = xml.etree.ElementTree.Element('component')
 component.set("name", "HAnim")
 component.set("level", "3")
 head.insert(0, component)
+
+humanoid = xml.etree.ElementTree.Element('HAnimHumanoid')
+humanoid.text = "\n"
+humanoid.tail = "\n"
+scene.insert(0, humanoid)
+humanoid_root = xml.etree.ElementTree.Element('HAnimJoint')
+humanoid_root.set("DEF", "root")
+humanoid_root.set("containerField", "skeleton")
+humanoid_root.text = "\n"
+humanoid_root.tail = "\n"
+humanoid.append(humanoid_root)
+humanoid_root_use = xml.etree.ElementTree.Element('HAnimJoint')
+humanoid_root_use.set("USE", "root")
+humanoid_root_use.set("containerField", "joints")
+humanoid_root_use.text = "\n"
+humanoid_root_use.tail = "\n"
+humanoid.append(humanoid_root_use)
+sacrum = xml.etree.ElementTree.Element('HAnimSegment')
+sacrum.text = "\n"
+sacrum.tail = "\n"
+humanoid_root.insert(0, sacrum)
+
+skullbase = xml.etree.ElementTree.Element('HAnimJoint')
+skullbase.set("DEF", "skullbase")
+skullbase.set("containerField", "children")
+skullbase.text = "\n"
+skullbase.tail = "\n"
+humanoid_root.append(skullbase)
+
+skullbase_use = xml.etree.ElementTree.Element('HAnimJoint')
+skullbase_use.set("USE", "skullbase")
+skullbase_use.set("containerField", "joints")
+skullbase_use.text = "\n"
+skullbase_use.tail = "\n"
+humanoid.append(skullbase_use)
+
 for cis in root.iter('CoordinateInterpolator'):
     DEF = cis.get("DEF")
     print(DEF)
@@ -43,6 +79,8 @@ for cis in root.iter('CoordinateInterpolator'):
     print(base)
     print(extension)
     new_node = xml.etree.ElementTree.Element('HAnimDisplacer')
+    new_node.text = "\n"
+    new_node.tail = "\n"
     displacements = []
     for i in range(len(base)):
         displacements.append(str(float(extension[i]) - float(base[i])))
@@ -54,7 +92,8 @@ for cis in root.iter('CoordinateInterpolator'):
         index = get_node_index(parent, cis)
         # replace the coordinate interpolator
         if index is not None:
-            parent.insert(index, new_node)
+            # parent.insert(index, new_node)
+            sacrum.append(new_node)
             parent.remove(cis)
 
             # replace set_fraction wiht weight
@@ -65,7 +104,7 @@ for cis in root.iter('CoordinateInterpolator'):
             # replace value_changed wiht weight
             routes = root.findall(".//ROUTE[@fromField='value_changed'][@fromNode='"+DEF+"']")
             for route in routes:
-                route.set("fromField", "weight")
+                # route.set("fromField", "weight")
                 # set to coordIndex of the new HAnimDisplacer node.
                 COORDDEF = route.get("toNode");
                 coords = root.findall(".//*[@DEF='"+COORDDEF+"']")
@@ -81,5 +120,15 @@ for cis in root.iter('CoordinateInterpolator'):
                             if point in pointsMatrix:
                                 coordIndex.append(str(i))
                         new_node.set("coordIndex", " ".join(coordIndex))
+                par = find_parent(root, route)
+                par.remove(route)
+
+for element in list(scene):
+    if not element.tag.startswith("HAnim") and element.tag != "ROUTE":
+        sacrum.append(element)
+        scene.remove(element)
+    elif element.tag == 'ROUTE':
+        scene.remove(element)
+        scene.append(element)
 
 X3D.write("../resources/Jaw_Drop_Output.x3d")
